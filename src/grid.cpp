077d483429e7aa5190f48650730dbbcba6a9e535
Yehor <egoros7x@yandex.ru>
2024-08-12 18:42:31 +0300

    migrate to sfml

--- src/grid.cpp
+++
@@ -1,6 +0,0 @@
-#include "vars.hpp"
-
-class Grid
-{
-	bool data[WINDOW_SIZE_X][WINDOW_SIZE_Y];
-};
--- src/main.cpp
+++ src/main.cpp
@@ -1,55 +1,19 @@
-#include "SDL.h"
-#include "vars.hpp"
+#include <SFML/Window.hpp>
 
-int main (int argc, char* argv[])
+int main ()
 {
-	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	sf::Window window(sf::VideoMode(800, 600), "Game of life");
+
+	while (window.isOpen())
 	{
-		printf("Failed to init SDL: %s\n", SDL_GetError());
-		return 1;
+			// check all the window's events that were triggered since the last iteration of the loop
+			sf::Event event;
+			while (window.pollEvent(event))
+			{
+				// "close requested" event: we close the window
+				if (event.type == sf::Event::Closed)
+					window.close();
+			}
 	}
-
-	// SDL_Surface* surface;
-	SDL_Window* window;
-	SDL_Renderer* renderer;
-
-	window = SDL_CreateWindow
-	(
-		 "Game of Life",
-		 SDL_WINDOWPOS_UNDEFINED,
-		 SDL_WINDOWPOS_UNDEFINED,
-		 WINDOW_SIZE_X,
-		 WINDOW_SIZE_Y,
-		 SDL_WINDOW_SHOWN
-	);
-
-	if (!window)
-	{
-		printf("Failed to create window: %s\n", SDL_GetError());
-		return 1;
-	}
-
-	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-
-	SDL_Rect rect = {20, 20, 10, 10};
-	
-	SDL_Event event;
-	while (true)
-	{
-		SDL_PollEvent(&event);
-		if (event.type == SDL_QUIT) { break; }
-			
-		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-		SDL_RenderClear(renderer);
-
-		SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-		SDL_RenderFillRect(renderer, &rect);
-		SDL_RenderPresent(renderer);
-	}	
-
-	SDL_DestroyRenderer(renderer);
-	SDL_DestroyWindow(window);
-	
-	SDL_Quit();
 	return 0;
 }
