d129c7181c79d670e8f37c84e338941af837003b
Yehor <egoros7x@yandex.ru>
2024-08-12 20:28:05 +0300

    add vsync and fix tabulation

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,18 @@
 int main ()
 {
 	sf::Window window(sf::VideoMode(800, 600), "Game of life");
+	window.setVerticalSyncEnabled(true);
 
 	while (window.isOpen())
 	{
-			// check all the window's events that were triggered since the last iteration of the loop
-			sf::Event event;
-			while (window.pollEvent(event))
-			{
-				// "close requested" event: we close the window
-				if (event.type == sf::Event::Closed)
-					window.close();
-			}
+		// check all the window's events that were triggered since the last iteration of the loop
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			// "close requested" event: we close the window
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
 	}
 	return 0;
 }
